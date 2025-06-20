644d06b00a95b09aaafb999e02584b2b0c611bf7
Gaurav <gaurav4332@gmail.com>
2025-06-20 17:36:28 +0530

    Exploring cured operation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 
+
+
+// This can only handle the get api calls 
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Gaurav",lastName:'Rajput'})
+})
+app.post("/user",(req,res)=>{
+  res.send('Data successfully save to the database ')
+})
+app.delete("/user",(req,res)=>{
+  res.send('Data successfully deleated to the database ')
+})
+
 app.use("/",(req,res)=>{
-    res.send('hello from the Dashboard ')
+    res.send('hello from the Dashboarddddddddddddddddddddddddddddddddddddd ')
 })
 app.use("/gaurav",(req,res)=>{
     res.send('hello from the server ')
